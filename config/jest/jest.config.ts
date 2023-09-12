65a302c52e6a9e269ad7051c952d826b758e2403
Alexandr-Horton <aleksandr.horton@gmail.com>
2023-09-12 22:32:03 +0300

    fix typing
    trig actions

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -8,6 +8,7 @@ import path from 'path';
 export default {
     globals: {
         __IS_DEV__: true,
+        __API__: '',
     },
     clearMocks: true,
     testEnvironment: 'jsdom',
@@ -29,14 +30,13 @@ export default {
         '<rootDir>src',
     ],
     testMatch: [
-        // Обнаружил разницу между МАК ОС и ВИНДОУС!!!
         '<rootDir>src/**/*(*.)@(spec|test).[tj]s?(x)',
     ],
     rootDir: '../../',
     setupFilesAfterEnv: ['<rootDir>config/jest/setupTests.ts'],
     moduleNameMapper: {
         '\\.s?css$': 'identity-obj-proxy',
-        '\\.svg': path.resolve(__dirname, 'jestEmptyComponent.tsx'),
+        '\\.svg': path!.resolve(__dirname, 'jestEmptyComponent.tsx'),
     },
     // Indicates whether the coverage information should be collected while executing the test
     // collectCoverage: false,
